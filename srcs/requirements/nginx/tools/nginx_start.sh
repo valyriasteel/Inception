5d6ede1ff4beeef86530201f5a7b523c738a6115
Berat Boşnak <110120659+valyriasteel@users.noreply.github.com>
2024-07-02 19:40:56 +0300

    Update nginx_start.sh

--- srcs/requirements/nginx/tools/nginx_start.sh
+++ srcs/requirements/nginx/tools/nginx_start.sh
@@ -2,7 +2,7 @@
 
 if [ ! -f /etc/ssl/certs/nginx.crt ]; then
 echo "Nginx: setting up ssl ...";
-openssl req -x509 -nodes -days 365 -newkey rsa:4096 -keyout /etc/ssl/private/nginx.key -out /etc/ssl/certs/nginx.crt -subj "/C=TR/ST=KOCAELI/L=GEBZE/O=42Kocaeli/CN=$WP_URL";
+openssl req -x509 -nodes -days 365 -newkey rsa:4096 -keyout /etc/ssl/private/nginx.key -out /etc/ssl/certs/nginx.crt -subj "/C=$COUNTRY/ST=$STATE/L=$LOCALITY/O=$ORGANIZATION/CN=$WP_URL";
 echo "Nginx: ssl is set up!";
 fi
 
